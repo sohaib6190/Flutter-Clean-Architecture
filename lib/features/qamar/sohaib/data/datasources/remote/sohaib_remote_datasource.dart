12e463f8cfa0c91db116f1eb60a1d5afa02b5168
Sohaib Qamar <qamarsohaib251@gmail.com>
2025-12-09 12:33:50 +0500

    fix post_view brick

--- lib/features/qamar/sohaib/data/datasources/remote/sohaib_remote_datasource.dart
+++
@@ -1,8 +0,0 @@
-import '../../models/request/sohaib_params.dart';
-import '../../models/response/sohaib_model.dart';
-
-abstract class SohaibRemoteDatasource {
-  SohaibRemoteDatasource();
-
-  Future<SohaibModel> fetchSohaib(SohaibParams params);
-}
--- lib/features/qamar/sohaib/domain/repositories/sohaib_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-
-import '../../../../../../app/errors/api_exceptions.dart';
-import '../../data/models/request/sohaib_params.dart';
-import '../../data/models/response/sohaib_model.dart';
-
-abstract class SohaibRepository {
-  Future<Either<Failure, SohaibModel>> fetchSohaib(SohaibParams params);
-}
