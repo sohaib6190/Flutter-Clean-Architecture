0eecc04b68b62ed6ccfa65fa4a4e8e50c7d49546
Sohaib Qamar <qamarsohaib251@gmail.com>
2025-12-09 13:05:54 +0500

    changes in get_view

--- tool_bloc/get_view/__brick__/lib/app/dependency_injection/{{name.snakeCase()}}_di.dart
+++ tool_bloc/get_view/__brick__/lib/app/dependency_injection/{{name.snakeCase()}}_di.dart
@@ -1,12 +1,12 @@
 import 'package:get_it/get_it.dart';
 
-import '../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/data/datasources/remote/{{name.snakeCase()}}_remote_datasource.dart';
-import '../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/data/datasources/remote/{{name.snakeCase()}}_remote_datasource_impl.dart';
-import '../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/data/repositories/{{name.snakeCase()}}_repository_impl.dart';
-import '../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/domain/facades/{{name.snakeCase()}}_use_cases_facade.dart';
-import '../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/domain/repositories/{{name.snakeCase()}}_repository.dart';
-import '../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/domain/usecases/{{name.snakeCase()}}/{{name.snakeCase()}}_usecase.dart';
-import '../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/presentation/blocs/{{name.snakeCase()}}_bloc.dart';
+import '../../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/data/datasources/remote/{{name.snakeCase()}}_remote_datasource.dart';
+import '../../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/data/datasources/remote/{{name.snakeCase()}}_remote_datasource_impl.dart';
+import '../../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/data/repositories/{{name.snakeCase()}}_repository_impl.dart';
+import '../../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/domain/facades/{{name.snakeCase()}}_use_cases_facade.dart';
+import '../../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/domain/repositories/{{name.snakeCase()}}_repository.dart';
+import '../../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/domain/usecases/{{name.snakeCase()}}/{{name.snakeCase()}}_usecase.dart';
+import '../../../features/{{module.snakeCase()}}/{{name.snakeCase()}}/presentation/blocs/{{name.snakeCase()}}_bloc.dart';
 
 final sl = GetIt.instance;
 
